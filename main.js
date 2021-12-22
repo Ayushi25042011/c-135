f4808b0d22e9fe706d97fac4acecb478ff37c254
Ayushi25042011 <95399293+Ayushi25042011@users.noreply.github.com>
2021-12-22 12:20:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 status="";
 object="";
 
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+}
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function modelLoaded()
+{
+	console.log("Model Loaded !");
+	status = true; 
+}
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+}
